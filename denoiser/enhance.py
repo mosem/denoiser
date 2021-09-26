72b6ad51a1620226c5222c6f521b2dce40f73e36
ortal <ortal1602@gmail.com>
2021-09-26 18:09:29 +0300

    fixed pesq and enhance issue

--- denoiser/enhance.py
+++ denoiser/enhance.py
@@ -149,14 +149,14 @@ def enhance(args, model=None, local_out_dir=None, loader=None):
             noisy_signals = noisy_signals.to(args.device)
             if args.device == 'cpu' and args.num_workers > 1:
                 pendings.append(
-                    pool.submit(_estimate_and_save,
+                    pool.submit(_estimate_and_save_new,
                                 model, noisy_signals, [f"{i}"], out_dir, args))
             else:
 
                 # Forward
                 estimate = get_estimate(model, noisy_signals, args)
                 noisy_sr = math.ceil(args.sample_rate / args.scale_factor)
-                save_wavs(estimate, noisy_signals, [f"{i}"], out_dir, noisy_sr=noisy_sr, enhanced_sr=args.sample_rate)
+                save_wavs_new(estimate, noisy_signals, [f"{i}"], out_dir, noisy_sr=noisy_sr, enhanced_sr=args.sample_rate)
 
         if pendings:
             print('Waiting for pending jobs...')
