821981209a114a6af9fb5c15d4a1e6575feaa7fd
mosem <mandelmoshe@gmail.com>
2021-10-18 12:28:27 +0300

    changed requirements file in tests.yml

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#SBATCH --gres=gpu:1 --mem=12G
-source /cs/labs/adiyoss/ortal1602/lab/bin/activate
-python /cs/usr/ortal1602/Desktop/MSc/ortal1602/projects/tmp_testing_BS/test_denoiser.py
