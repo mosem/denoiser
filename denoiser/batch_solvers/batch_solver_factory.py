b38082ea2f1254a84ad74ec01d38edb817f9282a
ortal <ortal1602@gmail.com>
2021-10-18 20:23:43 +0300

    added support in batch solver factory for demucs hifi bs

--- denoiser/batch_solvers/batch_solver_factory.py
+++ denoiser/batch_solvers/batch_solver_factory.py
@@ -1,4 +1,5 @@
 from denoiser.batch_solvers.demucs_bs import DemucsBS
+from denoiser.batch_solvers.demucs_hifi_bs import DemucsHifiBS
 
 
 class BatchSolverFactory:
@@ -7,5 +8,7 @@ class BatchSolverFactory:
     def get_bs(args):
         if args.experiment.model == "demucs":
             return DemucsBS(args)
+        elif args.experiment.model == "demucs_hifi":
+            return DemucsHifiBS(args)
         else:
             raise ValueError("Given model name is not supported")
