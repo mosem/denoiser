9da28cc34d902b9cefa8cca63297e8b185c2fd27
ortal <ortal1602@gmail.com>
2021-10-18 20:10:53 +0300

    added demucs_hifi batch solver and test configuration

--- test_denoiser.py
+++ test_denoiser.py
@@ -2,10 +2,14 @@ import subprocess
 
 OUTPUT_DIR = './outputs/tmp'
 
-TEST_COMMANDS = {'demucs': ['train.py', 'dset=valentini_dummy', 'experiment=demucs_1', 'stft_loss=True',
-                            'experiment.segment=2', 'experiment.stride=2','ddp=0', 'batch_size=16', 'experiment.scale_factor=2',
-                            'eval_every=1', 'epochs=2', f'hydra.run.dir={OUTPUT_DIR}'],
-                 }
+TEST_COMMANDS = {
+    'demucs': ['train.py', 'dset=valentini_dummy', 'experiment=demucs_1', 'stft_loss=True',
+               'experiment.segment=2', 'experiment.stride=2','ddp=0', 'batch_size=16', 'experiment.scale_factor=2',
+               'eval_every=1', 'epochs=2', f'hydra.run.dir={OUTPUT_DIR}'],
+    'demucs_hifi': ['train.py', 'dset=valentini_dummy', 'experiment=demucs_hifi_1',
+               'experiment.segment=2', 'experiment.stride=2','ddp=0', 'batch_size=16', 'experiment.scale_factor=2',
+               'eval_every=1', 'epochs=2', f'hydra.run.dir={OUTPUT_DIR}'],
+}
 REMOVE_OUTPUT_FILE_COMMAND = ['rm', '-r', OUTPUT_DIR]
 
 successful_tests = []
