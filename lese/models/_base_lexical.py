d2f3df9ff1a4f87eddffa9ac74e9253daef89c51
ortal1602 <or.tal1@mail.huji.ac.il>
2021-10-26 11:36:49 +0300

    removed files that should not be on master

--- lese/models/_base_lexical.py
+++
@@ -1,6 +0,0 @@
-class BaseLexical:
-    def __init__(self):
-        pass
-
-    def extract_feat(self, x):
-        pass
--- run_scripts/demucs_hifi_3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --gres=gpu:2
-
-source /cs/labs/adiyoss/ortal1602/lab/bin/activate
-module load cuda/10.1
-
-python /cs/labs/adiyoss/ortal1602/projects/BS/train.py dset=valentini  experiment.experiment_name=demucs_hifi_3 experiment=demucs_hifi experiment.hifi.l1_factor=5 experiment.hifi.gen_factor=5
-
