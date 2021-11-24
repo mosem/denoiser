fixed normalization

denoiser/models/autoencoder_composer.py
@@ -5,12 +5,13 @@ from denoiser.utils import capture_init
 class Autoencoder(nn.Module):
 
     @capture_init
-    def __init__(self, encoder, attention_module, decoder, skips):
+    def __init__(self, encoder, attention_module, decoder, skips, normalize):
         super().__init__()
         self.encoder = encoder
         self.attention_module = attention_module
         self.decoder = decoder
         self.skips = skips
+        self.normalize = normalize
 
     def estimate_valid_length(self, input_length):
         encoder_output_length = self.encoder.estimate_output_length(input_length)
@@ -19,6 +20,13 @@ class Autoencoder(nn.Module):
         return decoder_output_length
 
     def forward(self, signal):
+        if self.normalize:
+            mono = signal.mean(dim=1, keepdim=True)
+            std = mono.std(dim=-1, keepdim=True)
+            signal = signal / (self.floor + std)
+        else:
+            std = 1
+
         if self.skips:
             latent, skips_signals = self.encoder(signal)
             latent = self.attention_module(latent)
@@ -27,4 +35,6 @@ class Autoencoder(nn.Module):
             latent = self.encoder(signal)
             latent = self.attention_module(latent)
             out = self.decoder(latent)
-        return out
+
+
+        return std * out

denoiser/models/demucs_decoder.py
@@ -40,7 +40,6 @@ class DemucsDecoder(nn.Module):
                  resample=4,
                  growth=2,
                  max_hidden=10_000,
-                 normalize=True,
                  glu=True,
                  rescale=0.1,
                  floor=1e-3,
@@ -57,7 +56,6 @@ class DemucsDecoder(nn.Module):
         self.stride = stride
         self.floor = floor
         self.resample = resample
-        self.normalize = normalize
         self.scale_factor = scale_factor
 
         self.decoder = nn.ModuleList()
@@ -98,12 +96,6 @@ class DemucsDecoder(nn.Module):
         if signal.dim() == 2:
             signal = signal.unsqueeze(1)
 
-        if self.normalize:
-            mono = signal.mean(dim=1, keepdim=True)
-            std = mono.std(dim=-1, keepdim=True)
-            signal = signal / (self.floor + std)
-        else:
-            std = 1
         x = signal
 
         for decode in self.decoder:
@@ -120,4 +112,4 @@ class DemucsDecoder(nn.Module):
         else:
             pass
 
-        return std * x
+        return x