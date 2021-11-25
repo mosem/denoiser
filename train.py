bb23aeed381721b83d8bf60d9fc55ac8142bf4b7
mosem <mandelmoshe@gmail.com>
2021-11-25 19:22:35 +0200

    adding wandb online/offline/disabled flag.

--- train.py
+++ train.py
@@ -90,7 +90,8 @@ def _main(args):
 
     logger.info("For logs, checkpoints and samples check %s", os.getcwd())
     logger.debug(args)
-    wandb.init(project=WANDB_PROJECT_NAME, entity=WANDB_ENTITY, config=_get_wandb_config(args), group=args.experiment.experiment_name)
+    wandb.init(mode=args.wandb.mode, project=WANDB_PROJECT_NAME, entity=WANDB_ENTITY, config=_get_wandb_config(args),
+               group=args.experiment.experiment_name)
     if args.ddp and args.rank is None:
         start_ddp_workers()
     else:
