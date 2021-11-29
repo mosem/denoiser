87df1270f994a2b6a665066490d531328f32ee77
ortal1602 <or.tal1@mail.huji.ac.il>
2021-11-29 15:44:47 +0200

    added features support in Demucs and Autoencoder models.

--- denoiser/batch_solvers/batch_solver.py
+++ denoiser/batch_solvers/batch_solver.py
@@ -93,5 +93,7 @@ class BatchSolver(ABC):
             return 0
         with torch.no_grad():
             y_ft = self.ft_model.extract_feats(signal_to_extract_features_from)
-            estimated_embedded_dim = F.interpolate(estimated_embedded_dim, size=y_ft.shape, mode='bilinear')
-            return F.l1_loss(y_ft, estimated_embedded_dim) * self.ft_factor
+            if estimated_embedded_dim.shape[-2] != y_ft.shape[-2]:
+                estimated_embedded_dim = (torchaudio.transforms.Resample(estimated_embedded_dim.shape[-2], y_ft.shape[-2]).to(self.args.device)(estimated_embedded_dim.permute(0, 2, 1))).permute(0, 2, 1)
+            length = min(y_ft.shape[-1], estimated_embedded_dim.shape[-1])
+            return F.l1_loss(y_ft[...,:length], estimated_embedded_dim[...,:length]) * self.ft_factor
