ad5f8e8b0d928eb766ddc4dd3a1bbd85dbf4ff47
ortal1602 <or.tal1@mail.huji.ac.il>
2021-12-06 15:59:54 +0200

    added resume to wandb logging

--- train.py
+++ train.py
@@ -92,7 +92,7 @@ def _main(args):
     logger.debug(args)
     wandb_mode = os.environ['WANDB_MODE'] if 'WANDB_MODE' in os.environ.keys() else args.wandb.mode
     wandb.init(mode=wandb_mode, project=WANDB_PROJECT_NAME, entity=WANDB_ENTITY, config=_get_wandb_config(args),
-               group=args.experiment.experiment_name)
+               group=args.experiment.experiment_name, resume=(args.continue_from != ""))
     if args.ddp and args.rank is None:
         start_ddp_workers()
     else:
