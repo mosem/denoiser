26dd7fdf826c277d28504367d5ede211c481a5f6
Or Tal <or.tal@altair-semi.com>
2021-12-17 14:04:05 +0200

    removed prints

--- denoiser/stft_loss.py
+++ denoiser/stft_loss.py
@@ -25,7 +25,6 @@ def stft(x, fft_size, hop_size, win_length, window):
     Returns:
         Tensor: Magnitude spectrogram (B, #frames, fft_size // 2 + 1).
     """
-    print(f"\n\nx.shape: {x.shape}")
     x_stft = torch.stft(x, fft_size, hop_size, win_length, window)
     real = x_stft[..., 0]
     imag = x_stft[..., 1]
